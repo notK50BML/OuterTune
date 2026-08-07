9871c448fa4867b547e9efc16f4175c7cb0b56b3
K50BML <k50bml@gmail.com>
2026-08-07 18:50:43 +0800

    Update build.gradle.kts version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.dd3boh.outertune"
         minSdk = 24
         targetSdk = 36
-        versionCode = 83
-        versionName = "0.10.13"
+        versionCode = 86
+        versionName = "0.10.15"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         // expose the TagLib library version (from the version catalog) for the About screen
